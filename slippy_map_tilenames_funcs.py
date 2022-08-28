19a522de3c3853348a1388f84766612dddf6de4b
jonnymcmahon <jonathan.mcmahon11@gmail.com>
2022-08-28 16:18:28 +0100

    added context for funcs

--- slippy_map_tilenames_funcs.py
+++ slippy_map_tilenames_funcs.py
@@ -1,5 +1,6 @@
 import math
 
+#converts lat, long, and zoom to tile values for api query
 def deg2num(lat_deg, lon_deg, zoom):
   lat_rad = math.radians(lat_deg)
   n = 2.0 ** zoom
@@ -7,6 +8,7 @@ def deg2num(lat_deg, lon_deg, zoom):
   ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
   return (xtile, ytile)
 
+#converts lat, long, and zoom to tile float tile values for correct data selection
 def deg2numFloat(lat_deg, lon_deg, zoom):
   lat_rad = math.radians(lat_deg)
   n = 2.0 ** zoom
@@ -14,6 +16,7 @@ def deg2numFloat(lat_deg, lon_deg, zoom):
   ytile = (1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n
   return (xtile, ytile)
 
+#converts tile values (x,y) and zoom to degrees lat, long
 def num2deg(xtile, ytile, zoom):
   n = 2.0 ** zoom
   lon_deg = xtile / n * 360.0 - 180.0
